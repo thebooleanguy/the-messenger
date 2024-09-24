846e771e266632e35a6677b9653a674eacfdfec0
thebooleanguy <tharindus0601@gmail.com>
2024-09-24 20:59:24 +0530

    Add proper selected piece highlighting

--- board/chess_board.gd
+++ board/chess_board.gd
@@ -5,6 +5,8 @@ const PAWN := preload("res://pieces/Pawn.tscn")
 const TILE_SIZE := 63
 const CENTERED_TILE_OFFSET := Vector2(TILE_SIZE / 2, TILE_SIZE / 2)
 var tile_grid :Array = []
+var selected_piece = null
+var selected_tile = null
 
 func _ready() -> void:
 	draw_board()
@@ -19,7 +21,9 @@ func draw_board() -> void:
 		for x in range(8):
 			var tile := CHESS_TILE.instantiate()
 			tile.position = Vector2(x * TILE_SIZE, y * TILE_SIZE)
+			tile.grid_position = Vector2(x, y)
 			add_child(tile)
+			tile.connect("tile_clicked", _on_tile_clicked)
 			row.append(tile)
 		tile_grid.append(row)
 
@@ -37,12 +41,33 @@ func move_piece(curr_pos: Vector2, new_pos: Vector2) -> void:
 	var tile_from = tile_grid[curr_pos.y][curr_pos.x] 
 	var tile_to = tile_grid[new_pos.y][new_pos.x] 
 	var piece = tile_from.piece
-	
 	if piece:
 		piece.position = tile_to.position + CENTERED_TILE_OFFSET
 		tile_to.piece = piece
 		tile_from.piece = null
 
+func _on_tile_clicked(grid_pos: Vector2) -> void:
+	var clicked_tile = tile_grid[grid_pos.y][grid_pos.x]
+	
+	if selected_piece == null:
+		if clicked_tile.piece != null:
+			selected_piece = clicked_tile.piece
+			selected_tile = clicked_tile
+			highlight_tile(selected_tile, true)
+			clicked_tile.is_selected = true
+	else:
+		if clicked_tile != selected_tile:
+			move_piece(selected_tile.grid_position, grid_pos)
+			highlight_tile(selected_tile, false)
+			clicked_tile.is_selected = false
+			selected_piece = null
+			selected_tile = null 
+
+func highlight_tile(tile, highlight: bool) -> void:
+	if highlight:
+		tile.color_rect.color = Color(0.5, 0.5, 0.5, 0.8) 
+	else:
+		tile.color_rect.color = tile.color_rect_default_color
 
 func _process(delta: float) -> void:
 	pass
--- board/chess_tile.gd
+++ board/chess_tile.gd
@@ -3,7 +3,10 @@ extends Node2D
 @export var piece: Node = null
 @onready var color_rect := $Sprite2D/ColorRect
 @onready var color_rect_default_color = color_rect.color 
+var grid_position :Vector2 = Vector2.ZERO
+var is_selected: bool = false 
 
+signal tile_clicked(grid_position)
 
 func _ready() -> void:
 	pass
@@ -13,9 +16,18 @@ func _process(delta: float) -> void:
 
 
 func _on_color_rect_mouse_entered() -> void:
-	color_rect.color = Color(0.8, 0.8, 0.8, 0.5)
-	print(piece)
+	if not is_selected:
+		color_rect.color = Color(0.8, 0.8, 0.8, 0.5)
+		print(piece)
 
 
 func _on_color_rect_mouse_exited() -> void:
-	color_rect.color = color_rect_default_color
+	if not is_selected:
+		color_rect.color = color_rect_default_color
+
+
+func _on_color_rect_gui_input(event: InputEvent) -> void:
+	if event.is_action_pressed("select"):
+		tile_clicked.emit(grid_position)
+		#is_selected = true
+		color_rect.color = Color(0.5, 0.5, 0.5, 0.8)
