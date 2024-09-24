d94749e651675da81e2f5bc7cf6577ec477d7b4e
thebooleanguy <tharindus0601@gmail.com>
2024-09-24 19:03:45 +0530

    Refactor / simplify piece placement logic

--- board/chess_board.gd
+++ board/chess_board.gd
@@ -7,34 +7,23 @@ var tile_grid :Array = []
 
 func _ready() -> void:
 	draw_board()
-	set_piece(1, Vector2(0, 2))
-	draw_piece(Vector2(2,0))
 	#move_piece(Vector2(0,2), Vector2(0,4))
+	place_piece(BLACK_PAWN, Vector2(2,3))
 	print(tile_grid)
 
 
 func draw_board() -> void:
-	for row_y in range(8):
+	for y in range(8):
 		var row := []
-		for column_x in range(8):
+		for x in range(8):
 			var tile := CHESS_TILE.instantiate()
-			#tile.grid_position = Vector2(column_x, row_y)
-			tile.position = Vector2(column_x * TILE_SIZE, row_y * TILE_SIZE)
+			#tile.grid_position = Vector2(x, y)
+			tile.position = Vector2(x * TILE_SIZE, y * TILE_SIZE)
 			add_child(tile)
 			row.append(tile)
-			#tile_grid.append(tile)
 		tile_grid.append(row)
-			
-			#var black_pawn := BLACK_PAWN.instantiate()
-			#black_pawn.position = Vector2(column_x * TILE_SIZE + 32, row_y * TILE_SIZE + 32)
-			#add_child(black_pawn)
 	
-	
-func set_piece(piece: int, pos: Vector2) -> void:
-	tile_grid[pos.x][pos.y].piece = piece
-	#black_pawn.position = Vector2(7,7)
-	#tile_grid[pos.x][pos.y].add_child(black_pawn)
-	
+
 	
 func move_piece(curr_pos: Vector2, new_pos: Vector2) -> void:
 	#print('Before: ', tile_grid)
@@ -42,16 +31,14 @@ func move_piece(curr_pos: Vector2, new_pos: Vector2) -> void:
 	tile_grid[curr_pos.x][curr_pos.y].piece = 0
 	tile_grid[new_pos.x][new_pos.y].piece = piece
 	#print('After: ', tile_grid)
-
-
-func draw_piece(pos: Vector2) -> void:
-	var drawing_pos := Vector2(pos.x * TILE_SIZE + 32, pos.y * TILE_SIZE + 32)
-	var black_pawn := BLACK_PAWN.instantiate()
-	black_pawn.position = drawing_pos
-	#tile_grid[pos.x][pos.y].add_child(black_pawn)
-	add_child(black_pawn)
 	
 	
+func place_piece(piece_scene: PackedScene, pos: Vector2) -> void:
+	var tile = tile_grid[pos.y][pos.x]
+	var piece_instance := piece_scene.instantiate()
+	piece_instance.position = tile.position + Vector2(TILE_SIZE / 2, TILE_SIZE / 2)
+	add_child(piece_instance)
+	tile.piece = piece_instance
 
 func _process(delta: float) -> void:
 	pass
--- board/chess_tile.gd
+++ board/chess_tile.gd
@@ -1,8 +1,6 @@
 extends Node2D
 
-#var grid_position := Vector2.ZERO
-var piece := Pieces.NULL
-var team := Team.UNDEFINED
+var piece = null
 @onready var color_rect := $Sprite2D/ColorRect
 @onready var color_rect_default_color = color_rect.color 
 
@@ -18,27 +16,8 @@ func _process(delta: float) -> void:
 
 func _on_color_rect_mouse_entered() -> void:
 	color_rect.color = Color(0.8, 0.8, 0.8, 0.5)
-	print(piece, ' ' , team)
-	#print(grid_position, position)
-	#print(self)
+	print(piece)
 
 
 func _on_color_rect_mouse_exited() -> void:
 	color_rect.color = color_rect_default_color
-	
-	
-enum Pieces {
-	NULL, # 0
-	PAWN, # 1
-	ROOK, # 2
-	BISHOP, # 3
-	KNIGHT, # 4
-	QUEEN, # 5
-	KING # 6
-}
-
-enum Team {
-	UNDEFINED, # 0
-	BLACK, # 1
-	WHITE # 2
-}
